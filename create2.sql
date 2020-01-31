bdeee4bb148a69917188226e03548ac4a8bbe1f4
Tomasz Sapletta <t.sapletta@buchbinder.de>
2020-01-31 11:22:38 +0100

    naother structure, 2

--- create2.sql
+++ create2.sql
@@ -44,31 +44,41 @@ CREATE TABLE "main"."transaction" (
    "description" TEXT NOT NULL
 );
 
+-- HR, BOSS, WORKER1, WORKER2
 
 
--- konieczne stworzenie subkonta dla kazdego pracownika, gdzie w ciagu dnia / tygodnia / roku beda okreslane limity
--- w ten sposob, kazdy w cyklach bedzie mial ograniczone ilosci
+-- transakcje miedzy kontami i uzytkownikami
+CREATE TABLE "main"."transfer" (
+   "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 
--- dzieki wyslaniu od do, mozna ustalic limity na urlopy, dla kazdego usera oddzielnie,
--- sender_id, to moze byc subkonto usera, gdzie sa podane podstawowe ograniczenia
+   "sender_user_id" INTEGER NOT NULL,
+   "recipient_user_id" INTEGER NOT NULL,
 
--- przechowuje dla kazdego usera dane o jego ograniczeniach
+   "before_account_id" INTEGER NOT NULL,
+   "after_account_id" INTEGER NOT NULL,
 
--- zasada przy tworzeniu rekordow
--- kazdy rekord jest unikalny i moze byc jedynie dodany, nie mozna usuwac
--- jesli zostal popelniony blad, drugi wiersz powinien pozwolic na usuniecie tego bledu
--- k
+   "amount_id" INTEGER NOT NULL,
+   "description" TEXT NOT NULL
+);
+-- EXAMPLE
+-- from ASSIGNED to AVAILABLE
+-- from BOSS to WORKER, HR to WORKER
+-- question for special holiday, ASSIGNED = WORKER -> BOSS
+-- approve holiday, AVAILABLE = BOSS -> WORKER
+
+-- holidaybank
+-- Konto dla urlopow to samo jak pieniadze
+
+
+-- Definicje kont, jakie beda uzywane
+-- schemat procesow, jakie zachodza w firmie, to samo dla ticketow: PROBLEM->SOLUTION: QUESTION->ANSWER
 CREATE TABLE "main"."account" (
    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    "parent_id" INTEGER NOT NULL,
-   "localisation_id" INTEGER NOT NULL,
 
+-- ASSIGNED, AVAILABLE, USED, EXPIRED, PAYOUT
+   "name" TEXT NOT NULL,
    "description" TEXT NOT NULL
-
--- timestamp
-   "date" INTEGER NOT NULL,
--- created / deleted /mirror for another unit
-   "status" INTEGER NOT NULL DEFAULT (1),
 
 
 -- CONTRACT, ANNUAL, EXPIRED, REST
@@ -82,10 +92,45 @@ CREATE TABLE "main"."account" (
 -- -1 / +1
    "saldo_sign" INTEGER NOT NULL DEFAULT (1),
 
+}
+
+-- konieczne stworzenie subkonta dla kazdego pracownika, gdzie w ciagu dnia / tygodnia / roku beda okreslane limity
+-- w ten sposob, kazdy w cyklach bedzie mial ograniczone ilosci
+
+-- dzieki wyslaniu od do, mozna ustalic limity na urlopy, dla kazdego usera oddzielnie,
+-- sender_id, to moze byc subkonto usera, gdzie sa podane podstawowe ograniczenia
+
+-- przechowuje dla kazdego usera dane o jego ograniczeniach
+
+-- zasada przy tworzeniu rekordow
+-- kazdy rekord jest unikalny i moze byc jedynie dodany, nie mozna usuwac
+-- jesli zostal popelniony blad, drugi wiersz powinien pozwolic na usuniecie tego bledu
+-- k
+CREATE TABLE "main"."amount" (
+   "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   "parent_id" INTEGER NOT NULL,
+   "localisation_id" INTEGER NOT NULL,
+
+   "description" TEXT NOT NULL
+
+-- timestamp
+   "date" INTEGER NOT NULL,
+-- created / deleted /mirror for another unit
+   "status" INTEGER NOT NULL DEFAULT (1),
+
+-- Dla wymiany walut, to moze byc przydatne rowniez, na ktory dzien byl kurs brany pod uwage
+
+-- rok, dla ktorego jest jednostka aktualna
+   "unit_year" INTEGER NOT NULL,
+-- dzien roku, tydzien roku, miesiac roku
+   "unit_no" INTEGER NOT NULL,
+-- WORKDAY, WORKWEEK, WORKMONTH
    "unit_name" TEXT NOT NULL,
--- 1, 10 , 1000, 1000 000
+-- 1, 7 , 31, 1000 000
    "unit_factor" INTEGER NOT NULL DEFAULT (1),
+-- wartosc z przecinkami, float, +/-
    "unit_value" REAL NOT NULL DEFAULT (1),
+
 );
 
 
